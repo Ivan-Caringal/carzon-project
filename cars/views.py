8ca572348dbc25c51df66c1b93ef0793d7ba1f4c
Ivan-Caringal <ivancaringal20@gmail.com>
2025-02-26 23:26:01 +0800

    to do: pagination

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Car
 
 # Create your views here.
 def cars(request):
-    return render(request, 'cars/cars.html')
+    cars = Car.objects.order_by('-created_date')  # ✅ Correct indentation
+    data ={
+        'cars': cars,
+    }
+    return render(request, 'cars/cars.html', data)
+
+
 
 def car_detail(request, id):
     single_car = get_object_or_404(Car, pk=id)
